606ba01ac8429256fc2218e7de34e1cf263ac5f4
Zoé Canoen <zoe.canoen@gmail.com>
2018-12-02 16:35:47 +0100

    Ajout temporaire de la navBar
    La navBar ne doit pas etre sur la premiere connexion, et doit etre differente en fonction des connexions choisie

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
 <template>
   <div class="container">
+
+
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-dark bg-dark fixed-top">
       <router-link to="/">
         <img src="./assets/burger-flat.png" width="30px" height="30px" class="d-inline-block align-top" alt=""/>
       </router-link>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Menu
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <router-link class="dropdown-item" to="/menu">Voir le menu</router-link>
+              <div class="dropdown-divider"></div>
+              <router-link class="dropdown-item" to="/changeMenu">Modifier le Menu</router-link>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Service
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <router-link class="dropdown-item" to="/order-1">Voir les commandes</router-link>
+              <router-link class="dropdown-item" to="pages/service/takeOrder">Prendre une commande</router-link>
+              <div class="dropdown-divider"></div>
+              <router-link class="dropdown-item" to="pages/service/bill">Régler l'addition</router-link>
+            </div>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Cuisine
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <router-link class="dropdown-item" to="/cookerOrder">Voir les commandes</router-link>
+              <router-link class="dropdown-item" to="/rolesCookers">Accéder aux rôles</router-link>
+              <router-link class="dropdown-item" to="/unavailableDish">Plat indisponible</router-link>
+              <div class="dropdown-divider"></div>
+              <router-link class="dropdown-item" to="/modifiedRoles">Modifier les rôles</router-link>
+            </div>
+          </li>
+        </ul>
+      </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
