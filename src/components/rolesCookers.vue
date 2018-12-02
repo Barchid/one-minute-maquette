dadab45918657fd1d9668a8b584947750b533501
Zoé Canoen <zoe.canoen@gmail.com>
2018-12-02 13:25:13 +0100

    Correctif : les pages d'accueils s'affichent

--- src/components/rolesCookers.vue
+++ src/components/rolesCookers.vue
@@ -39,7 +39,7 @@
         </div>
         <div class="row">
           <div class="col-md">
-            Garde manger
+
           </div>
           <div class="col-md">
             Commis
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-
+import Accueil from './views/Accueil.vue';
 Vue.use(Router);
 
 export default new Router({
@@ -22,12 +22,9 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
     {
-      path: '/accueil',
-      name: 'accueil',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Accueil.vue'),
-    },
+          path: '/accueil',
+          name: 'accueil',
+          component: Accueil,
+        },
   ],
 });
